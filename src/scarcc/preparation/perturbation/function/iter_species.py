dcd1c0f544ac93d4f6273321b2c951ea7f9e0136
TFwongw <wong0755@umn.edu>
2024-02-20 10:09:16 -0600

    update doc

--- src/scarcc/preparation/perturbation/function/iter_species.py
+++ src/scarcc/preparation/perturbation/function/iter_species.py
@@ -1,7 +1,33 @@
-"""iterate items to passinto function""" 
+"""iterate items to pass into function""" 
 
 def iter_species(models,f,*args,**kwargs): 
-    """Factory for passing extra_objects iterate with model into functions."""
+    """Factory for passing extra_objects iterate with model into functions.
+    
+    Parameters
+    ----------
+    models : list or zip
+        List of models or zip of models and extra_objects that will iterate together.
+    f : function
+        Function to be called with in put as model and extra_objects.
+    *args : positional arguments following models or models and extra_objects
+    **kwargs : keyword arguments
+
+    Returns
+    -------
+    r_object : list
+        List of return objects from function f.
+
+    Examples
+    --------
+    # print model names
+    >>> _ = iter_species([E0, S0], print)
+    E0
+    S0 
+    # print model names and corresponding alpha as extra_objects
+    >>> _ = iter_species(zip([E0, S0],[20,10]), print)
+    E0 [20]
+    S0 [10]
+    """
     def simple_iter():
         for model in models:
             r_object.append(f(model,*args,**kwargs))
@@ -11,7 +37,7 @@ def iter_species(models,f,*args,**kwargs):
         for model, *extra_objects in models: 
             r_object.append(f(model,extra_objects,*args,**kwargs))
 
-    r_object = list()
+    r_object = []
     iter_fun = coupled_iter if isinstance(models, zip) else simple_iter
     iter_fun()
     return(r_object)
