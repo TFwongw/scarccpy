707589122d652bf7d51646f6b03c0da1bd727b98
TFwongw <wong0755@umn.edu>
2024-02-20 11:06:36 -0600

    directory search for Data and model

--- src/scarcc/preparation/find_directory.py
+++ src/scarcc/preparation/find_directory.py
@@ -1,6 +1,6 @@
 import os
 def find_directory(desired_directory_name, script_filepath): # start with where the script is located
-    """Find directory in the current directory or in the parent directory. 
+    """Find directory in the current directory or up maximum 2 levels of directory. 
     If not found, make directory at script location.
     
     Parameters
@@ -15,13 +15,26 @@ def find_directory(desired_directory_name, script_filepath): # start with where 
     Returns
     -------
     directory_path : str
-        Path to the desired directory.
+
     """
-    start_directory = os.path.dirname(script_filepath)
-    print(os.path.dirname(start_directory))
-    directory_path = os.path.join(start_directory, desired_directory_name)
-    if not os.path.isdir(directory_path):
-        print(f'Directory {desired_directory_name} not exist, make directory at script location')
-        directory_path = os.path.join(os.path.abspath(start_directory), desired_directory_name) 
-        os.makedirs(directory_path)
+
+    def check_directory(directory_path):
+        return os.path.isdir(directory_path)
+    
+    start_directory = os.path.abspath(script_filepath)
+    current_directory = start_directory
+
+    max_depth = 2
+    while os.path.basename(current_directory) != 'scarccpy' and max_depth > 0:
+        # Move up one level
+        current_directory = os.path.dirname(current_directory)
+        max_depth -= 1
+
+        directory_path = os.path.join(current_directory, desired_directory_name)
+        if check_directory(directory_path):
+            return directory_path
+
+    print(f'Directory {desired_directory_name} not exist, make directory at script level.')
+    directory_path = os.path.join(os.path.dirname(start_directory), desired_directory_name)
+    os.makedirs(directory_path)
     return directory_path
