2e5fbd3548da22d379bf9532a1986b21fdc336cd
TFwongw <wong0755@umn.edu>
2024-02-21 20:31:31 -0600

    update Sphinx requirement

--- scripts/sim_COMETS.py
+++ scripts/sim_COMETS.py
@@ -1,10 +1,8 @@
 import logging
 
-logging.basicConfig(filename='cabbcs.log', 
+logging.basicConfig(filename='scarcc_simulation.log',
                     level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     filemode='w')
 
-
-logging.debug('This message should go to the')
