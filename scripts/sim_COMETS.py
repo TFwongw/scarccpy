d51bee6568e06e71f5be15f3c385866ddca00a21
TFwongw <wong0755@umn.edu>
2024-03-14 14:48:44 -0500

    Script Example

--- scripts/sim_COMETS.py
+++ scripts/sim_COMETS.py
@@ -1,18 +1,17 @@
 import logging
 import pandas as pd
 import os
-
-import itertools
 import cometspy as c
-from scarcc.preparation.find_directory import find_directory
+
 from scarcc.preparation.metabolic_model import BasicModel
-from scarcc.sim_engine.simulation_workflow import SimulateCombinedAntibiotics
+from scarcc.preparation.find_directory import find_directory
+from scarcc.sim_engine.simulation_workflow import run_sim_workflow
 
 p = c.params()
 p.set_param("defaultKm", 0.00001) # M
 p.set_param("defaultVmax", 10) #mmol/gDw/hr
 p.set_param("maxCycles", 30)
-# p.set_param("maxCycles", 150)
+# p.set_param("maxCycles", 550)
 p.set_param("timeStep", 1)
 p.set_param('writeFluxLog', True)
 
@@ -24,21 +23,20 @@ logging.basicConfig(filename='scarcc_simulation.log',
 
 model_directory = find_directory('models', os.path.abspath(''))
 data_directory = find_directory('Data', os.path.abspath(''))
-chamber_directory = find_directory('SimChamber', os.path.abspath(''))
-
-model_directory = '../models'
-data_directory = '../Data'
-chamber_directory = '../SimChamber'
+sim_chamber_directory = find_directory('SimChamber', os.path.abspath(''))
 
 # initialize model
 E0, S0, all_components = BasicModel(model_directory=model_directory, flux_weighting=True).load_ES_models()
-# alpha_table = pd.read_csv(os.path.join(data_directory, 'alpha_table_m1.csv'), index_col=0)
-alpha_table = pd.DataFrame([[2000], [40]], index=['E0', 'S0'], columns=['folA']).T
 
-cas = SimulateCombinedAntibiotics(current_gene='folA', E0=E0, S0=S0, alpha_table=alpha_table,
-                                p=p, mono=True, base=chamber_directory)
+SG_list = ['folA', 'folP'] # Optional, Example: ['folA', 'folP', 'folC']
+DG_list = [['folA', 'folP']] # Optional, Example: [['folA', 'folP'], ['folC', 'folP'], ['folA', 'folC']]
+method_list = ['m1']
+simulation_kwargs = {
+    'E0': E0,
+    'S0': S0,
+    'base': sim_chamber_directory,
+    'p': p}
 
-biomass_df_list, flux_df_list = cas.get_BM_df()
-
-pd.concat(biomass_df_list, axis=1).to_csv(os.path.join(data_directory, 'test_biomass_df.csv'))
-pd.concat(itertools.chain(*flux_df_list)).to_csv(os.path.join(data_directory, 'test_flux_df.csv'))
+# Pass SG_list too if wish to overwrite preexisting SG files in Data directory
+df_container = run_sim_workflow(method_list=method_list, data_directory=data_directory,
+                                DG_list=DG_list, **simulation_kwargs)
