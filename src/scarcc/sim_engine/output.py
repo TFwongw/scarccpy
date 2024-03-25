794160cdb23221a0b58c76cf3e0955a1e1f13559
TFwongw <wong0755@umn.edu>
2024-03-25 17:01:02 -0500

    src: drop is_DG boolean

--- src/scarcc/sim_engine/output.py
+++ src/scarcc/sim_engine/output.py
@@ -1,3 +1,5 @@
+"""Module for producing output data from simulation and saving in data directory"""
+
 import pandas as pd
 import os
 import itertools
@@ -10,9 +12,7 @@ from scarcc.preparation.target_gene.gene_format_handler import GeneFormatHandler
 logger = logging.getLogger(__name__)
 
 def merge_single_gene_gr(sg_df, dg_df):
-    # gcomb as index, retrive the corresponding single gene in sg_df
-    # ? Normal col && normal str correspondence in dict converion first for checker board?
-    
+    """Merge single gene growth rate data from either first or second gene in drug combination as supplied in dg_df index"""
     dg_df = pd.concat([sg_df.loc[['Normal']], dg_df]).rename(index={'Normal': 'Normal.Normal'})
     dg_df['First_gene'], dg_df['Second_gene'] = list(zip(*dg_df.index.map(lambda x: GeneFormatHandler(x).SG)))
     dg_df.rename(index={'Normal.Normal': 'Normal'}, inplace=True)
@@ -22,15 +22,31 @@ def merge_single_gene_gr(sg_df, dg_df):
                 )
     return dg_df
 
-def add_additive_and_drug_comb_response(df): # after normalization of DG_gr
+def add_additive_and_drug_comb_response(df, additive_threshold=0.05): # after normalization of DG_gr
+    """Add columns for classification of drug combination response based on sign of difference between observed and predicted values.
+    
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame containing growth rate data for drug combination experiments.
+    additive_threshold : float, optional
+        Threshold for classifying drug combination response as additive. Default is 0.05.
+        
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame with added columns for classification of drug combination response.
+    """
     for sp_cul in itertools.product(['E0', 'S0'], ['coculture', 'monoculture']):
         sp_cul = '_'.join(sp_cul)
         df[f'Predicted_additive_effect_{sp_cul}'] = df[f'{sp_cul}_First_gene'] * df[f'{sp_cul}_Second_gene']
-        df[f'po_diff_{sp_cul}'] = df[sp_cul] - df[f'Predicted_additive_effect_{sp_cul}'] # ! op_col
-        df[f'Drug_comb_effect_{sp_cul}'] = convert_po_col(df[f'po_diff_{sp_cul}'], additive_threshold=0.05)
+        # df[f'op_diff_{sp_cul}'] = df[sp_cul] - df[f'Predicted_additive_effect_{sp_cul}'] # TODO: call convert_op_col
+        df[f'po_diff_{sp_cul}'] = df[f'Predicted_additive_effect_{sp_cul}'] - df[sp_cul] # predicted - observed
+        df[f'Drug_comb_effect_{sp_cul}'] = convert_po_col(df[f'po_diff_{sp_cul}'], additive_threshold=additive_threshold)
     return df
 
 def reorder_columns(df):
+    """Reorder columns in DataFrame if they are present"""
     column_order = ['E0_coculture', 'S0_coculture',
     'Predicted_additive_effect_E0_coculture',
     'Predicted_additive_effect_S0_coculture', 'E0_monoculture',
@@ -47,62 +63,121 @@ def reorder_columns(df):
     return df.reindex(columns=[col for col in column_order if col in df.columns])
 
 class DrugCombinationEffectClassification:
-    def __init__(self, dfs_in_SG_layer, dfs_in_DG_layer):
+    """Class for adding columns for classification of drug combination response with corresponding growth rate data in single target gene given
+    
+    Parameters
+    ----------
+    dfs_in_SG_layer : dict
+        Dictionary containing data frames for single gene experiments.
+    dfs_in_DG_layer : dict
+        Dictionary containing data frames for drug combination experiments.
+    additive_threshold : float, optional
+        Threshold for classifying drug combination response as additive. Default is 0.05.
+
+    Attributes
+    ----------
+    dfs_in_SG_layer : dict
+        Dictionary containing data frames for single gene experiments.
+    dfs_in_DG_layer : dict
+        Dictionary containing data frames for drug combination experiments, if not None, add drug combination response classification.
+    pure_dg_gr : pd.DataFrame
+        DataFrame containing growth rate data for drug combination experiments before merging with single gene growth rate data.
+    additive_threshold : float
+        Threshold for classifying drug combination response as additive.
+    """
+    def __init__(self, dfs_in_SG_layer, dfs_in_DG_layer, additive_threshold=0.05):
         self.dfs_in_SG_layer = dfs_in_SG_layer
         self.dfs_in_DG_layer = dfs_in_DG_layer
-        self.generate_DG = dfs_in_DG_layer is not None
         self.pure_dg_gr = None
+        self.additive_threshold = additive_threshold
 
-    # construct a sub her, normalize df
     def get_gr_df(self):
+        """Add gr_df to the container dict from biomass
+
+        1. identify Normal case as normalizing factor
+        2. merge single gene gr data to dg gr data
+        3. reorder columns
+        """
         sg_df = get_growth_rate_df(self.dfs_in_SG_layer['biomass'])
         if 'Normal' not in sg_df.index:
             Normal_row = sg_df.loc[[index for index in sg_df.index if '0.0' in index][0]]
             sg_df.loc['Normal'] = Normal_row
         self.dfs_in_SG_layer['growth_rate'] = reorder_columns(sg_df)
-        if self.generate_DG:
+        if self.dfs_in_DG_layer is not None:
             dg_df = get_growth_rate_df(self.dfs_in_DG_layer['biomass'])
-            self.pure_dg_gr = dg_df.copy() # This is before merge, because joining sg_df in non-normalized and normalized 
+            self.pure_dg_gr = dg_df.copy() # This is before merge, because joining sg_df in non-normalized and normalized
 
             dg_df = merge_single_gene_gr(sg_df, dg_df)
             self.dfs_in_DG_layer['growth_rate'] = reorder_columns(dg_df)
 
     def get_normalized_gr_df(self):
-        # sg_df = self.dfs_in_SG_layer['growth_rate'].copy() # reassignment fo not affect non-normalized gr
+        """Add normalized_gr_df to the container dict from growth_rate
+
+        1. normalize growth rate data by dividing by Normal row
+        2. merge single gene gr data to dg gr data
+        3. reorder columns
+        """
         sg_df = self.dfs_in_SG_layer['growth_rate']
         Normal_row = sg_df.loc['Normal']
         sg_df = sg_df.div(Normal_row)
         self.dfs_in_SG_layer['normalized_growth_rate'] = reorder_columns(sg_df)
             
-        if self.generate_DG:
+        if self.dfs_in_DG_layer is not None:
             dg_df = self.pure_dg_gr
             dg_df = dg_df.div(Normal_row)
-            dg_df = merge_single_gene_gr(sg_df, dg_df)
-            dg_df = add_additive_and_drug_comb_response(dg_df)
+            dg_df = merge_single_gene_gr(sg_df, dg_df) # merged one is normalized
+            dg_df = add_additive_and_drug_comb_response(dg_df, additive_threshold=self.additive_threshold)
             self.dfs_in_DG_layer['normalized_growth_rate'] = reorder_columns(dg_df)
             self.dfs_in_DG_layer['drug_response_classification'] = dg_df.filter(regex='Drug_comb_effect_')
 
 class MethodDataFiller:
+    """Class for filling df_container dict with tuple (method, XG) as keys
+
+    Parameters
+    ----------
+    df_container : dict
+        Dictionary containing data frames for single gene and drug combination experiments.
+    data_directory : str
+        Path to directory for saving output data.
+    additive_threshold : float, optional
+        Threshold for classifying drug combination response as additive. Default is 0.05.
+
+    Attributes
+    ----------
+    df_container : dict
+        Dictionary containing data frames for single gene and drug combination experiments.
+    data_directory : str
+        Path to directory for saving output data.
+    methods : set
+        Set of methods in df_container.
+    XGs : set
+        Set of XGs in df_container.
+    additive_threshold : float
+        Threshold for classifying drug combination response as additive.
+    """
     # TODO: handler for not supplying SG biomass, OR handle for the first construction of container dict
-    # ! Is in fact op diff rather than po diff - rename column
-    # for each method, cal the ~. , og idea go into the method, with SG and DG as sub dict
-    # one level dict - query method as first element in tuple
-    # ！if DG none then no cal any for DG OR ensure both SG and DG exist
-    def __init__(self, df_container, data_directory): #? make a base accept only sg, dg
+    # TODO: if DG none then no cal any for DG OR ensure both SG and DG exist
+    def __init__(self, df_container, data_directory, additive_threshold): #? make a base accept only sg, dg
         self.df_container = df_container
         self.data_directory = data_directory
         self.methods = set([key[0] for key in df_container.keys()]) # first element of tuple is method
         self.XGs = set([key[1] for key in df_container.keys()]) # DG or SG
+        self.additive_threshold = additive_threshold
 
     def fill_container(self):
+        """Fill df_container with drug combination effect classification data frames for each method and XG in container dict."""
         for method in self.methods:
-            dcd = DrugCombinationEffectClassification(self.df_container[method, 'SG'], self.df_container[method, 'DG']) # each key level are dict of dfs
+            dcd = DrugCombinationEffectClassification(self.df_container[method, 'SG'], self.df_container[method, 'DG'], self.additive_threshold) # each key level are dict of dfs
             dcd.get_gr_df()
             dcd.get_normalized_gr_df()
         return self.df_container
     
     def write_to_csv(self):
+        """Save data frames in df_container in data directory."""
         def filename_format(data_directory, method, XG, df_type):
+            """Format file name for saving data frames in data directory.
+            File will also be used in data_analysis section for carbon allocation
+            """
             if df_type == 'biomass':
                 return os.path.join(data_directory, f'BM_{XG}_{method}.csv')
             if df_type == 'flux':
