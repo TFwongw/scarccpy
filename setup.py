1c009ca3d4e669bd1c030175b3bcd96b63e74609
TFwongw <wong0755@umn.edu>
2024-03-26 10:55:32 -0500

    regen pyreverse package html

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scarcc',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/TFwongw/scarccpy',
     author='Thomas Wong',
     author_email='wong0755@umn.edu',
